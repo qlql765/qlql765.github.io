d8c4a5f14124982767f434bc4afdd3f84a16eb1c
qlql765 <353054881@qq.com>
2022-10-15 17:21:13 +0800

    js

--- js/lib.dz/drpy.js
+++ js/lib.dz/drpy.js
@@ -1,4 +1,4 @@
-import 'https://jihulab.com/qlql765/q/-/raw/master/js/lib.dz/util.ym.js'
-import dr from 'https://jihulab.com/qlql765/q/-/raw/master/js/lib.dz/drpy.min.js'
+import 'https://qlql765.github.io/js/lib.dz/util.ym.js'
+import dr from 'https://qlql765.github.io/js/lib.dz/drpy.min.js'
  
 __JS_SPIDER__ = dr.DRPY()
