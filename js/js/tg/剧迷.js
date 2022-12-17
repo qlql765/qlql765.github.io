1cafe422427779d28b9335af2131d7eaabe7f4d0
qlql765 <108382776+qlql765@users.noreply.github.com>
2022-12-17 09:39:12 +0800

    drpy

--- js/js/tg/剧迷.js
+++ js/js/tg/剧迷.js
@@ -1,7 +1,7 @@
 muban.首图.二级.tabs = '.myui-panel__head.bottom-line h3';
 var rule = Object.assign(muban.首图,{
 title:'剧迷',
-host:'https://gmtv1.xyz',
+host:'https://gmtv2.xyz',
 url:'/genre/fyclass---fypage.html',
 searchUrl:'/search/-------------.html?wd=*&submit=',
 class_name:'电视剧&电影&综艺&动漫',//静态分类名称拼接
